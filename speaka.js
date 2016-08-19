b18c889302a200c9d929152540cdbfa2fd5e0169
Daniel Irwin <daniel.irwin@enernoc.com>
2016-08-18 23:07:53 -0400

    remove speaker as its unused and a pain to install

--- speaka.js
+++ speaka.js
@@ -1,36 +1,36 @@
-/**
- * Created by daniel.irwin on 8/7/16.
- */
-var cameras = require('./cameras.json');
-var cameraManager = require('./IPCameraManager')(cameras);
-
-
-var Speaker = require('speaker');
-
-// Create the Speaker instance
-var speaker = new Speaker({
-    channels: 1,          // 2 channels
-    bitDepth: 16,         // 16-bit samples
-    sampleRate: 16000     // 44,100 Hz sample rate
-});
-
-var grapher = require('./grapher');
-
-cameraManager.getCamera({
-    name : "kitchen"
-}, (err, cam) => {
-    if(!err) {
-        var stream = cameraManager.proxyAudio(cam);
-
-        stream.pipe(speaker);
-
-        stream.on('data', function (data) {
-
-            //console.log('', data.readUInt8());
-
-            //grapher.graph(data);
-
-        });
-    }
-});
-
+///**
+// * Created by daniel.irwin on 8/7/16.
+// */
+//var cameras = require('./cameras.json');
+//var cameraManager = require('./IPCameraManager')(cameras);
+//
+//
+//var Speaker = require('speaker');
+//
+//// Create the Speaker instance
+//var speaker = new Speaker({
+//    channels: 1,          // 2 channels
+//    bitDepth: 16,         // 16-bit samples
+//    sampleRate: 16000     // 44,100 Hz sample rate
+//});
+//
+//var grapher = require('./grapher');
+//
+//cameraManager.getCamera({
+//    name : "kitchen"
+//}, (err, cam) => {
+//    if(!err) {
+//        var stream = cameraManager.proxyAudio(cam);
+//
+//        stream.pipe(speaker);
+//
+//        stream.on('data', function (data) {
+//
+//            //console.log('', data.readUInt8());
+//
+//            //grapher.graph(data);
+//
+//        });
+//    }
+//});
+//
